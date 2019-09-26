6345899ce06264d2e0a4b5f43c78515d2f0ce4aa
phamngochau1506@gmail.com <haupham156>
2019-09-26 09:25:08 +0700

    BMI

---
+++ src/BMI.java
@@ -0,0 +1,29 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class BMI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double weight, height, bmi;
+        System.out.println("Enter Weight: ");
+        weight = sc.nextDouble();
+        System.out.println("Enter Height: ");
+        height = sc.nextDouble();
+
+        bmi = weight / Math.pow(height, 2);
+
+        // String textFOrmat = String.format("output [%s]","text1");
+
+        System.out.printf("%-20s%s", "bmi", "Interpretation\n");
+
+        if (bmi < 18) {
+            System.out.printf("%-20.2f%s", bmi, "Underweight");
+        } else if (bmi < 25.0) {
+            System.out.printf("%-20.2f%s", bmi, "Normal");
+        } else if (bmi < 30.0) {
+            System.out.printf("%-20.2f%s", bmi, "Overweight");
+        } else {
+            System.out.printf("%-20.2f%s", bmi, "Obese");
+        }
+    }
+}
